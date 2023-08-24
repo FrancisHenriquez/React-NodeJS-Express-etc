a8cc1970dea46295bdd0fb2bc10edc3b42e393bb
Francis Henriquez Matos <francishenriquez07@gmail.com>
2023-08-24 14:28:17 -0400

    first commit

---
+++ src/components/GoalList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const GoalList = () => {
+    return (
+        <ul className='goal-list'>
+            <li>Finish the Course</li>
+            <li>How do you know? HOW DO YOU KNOW!?</li>
+            <li>There shall be ragnarok </li>
+        </ul>
+    )
+}
